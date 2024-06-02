d1c6cbdd5bc8965e7a242fc134f86b7f3c6524d2
kevinyeungkevinyeung <kevinyeungkevinyeung@hotmail.com>
2024-06-02 21:50:57 +0800

    understaning error

--- understanding_error/src/main.rs
+++ understanding_error/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
+use std::fs::{ self, File };
 use std::io::ErrorKind;
-use std::io::{self, Read};
+use std::io::{ self, Read };
 
 fn main() {
     let greeting_file_result = File::open("Hello.text");
@@ -15,15 +15,18 @@ fn main() {
     let greeting_file_result = File::open("hello.txt");
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem createing the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error);
+        Err(error) =>
+            match error.kind() {
+                // get a error enum back for matching
+                ErrorKind::NotFound =>
+                    match File::create("hello.txt") {
+                        Ok(fc) => fc,
+                        Err(e) => panic!("Problem createing the file: {:?}", e),
+                    }
+                other_error => {
+                    panic!("Problem opening the file: {:?}", other_error);
+                }
             }
-        },
     };
 
     let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
@@ -36,15 +39,18 @@ fn main() {
         }
     });
 
-    let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file = File::open("hello.txt").unwrap(); // if return ok, it will return the file, else it will panic
 
-    let greeting_file =
-        File::open("hello.txt").expect("hello.txt should be included in this project");
+    let greeting_file = File::open("hello.txt").expect(
+        "hello.txt should be included in this project"
+    );
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username)
+    //let mut username = String::new();
+    //File::open("hello.txt")?.read_to_string(&mut username)?; // ? if error return error, else return the file
+    //Ok(username)
+
+    //? operator can only be used in a function that returns Result or Option
+    fs::read_to_string("hello.txt")
 }
